5843b3f2d94cd3ac5f70516eea61ead28ddc75cb
Jason Fleming <45003594+jasonfleming1@users.noreply.github.com>
2019-04-26 19:54:00 -0500

    added two more uses
    Added more users and fixed broken default image link

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,13 +5,18 @@
 var friends = [
     {
         'name': 'Ahmed',
-        'photo':"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        'photo':"https://en.wikipedia.org/wiki/Ahmed_I#/media/File:Sultan_I._Ahmet.jpg",
         'scores':[5, 1, 4, 4, 5, 1, 2, 5, 4, 1,]
     },
     {
-        'name': 'Stevie Steve',
-        'photo':"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        'scores':[1, 1, 1, 1, 1, 1, 1, 1, 1, 1,]
+        'name': 'Bill Doe',
+        'photo':"https://www.energy.gov/sites/prod/files/styles/borealis_contributor_hero_respondxl/public/Bill_0.jpg?itok=rXE0vQPR",
+        'scores':[1, 1, 4, 1, 3, 1, 1, 1, 2, 1,]
+    },
+        {
+        'name': 'Jimmy Diggs',
+        'photo':"http://weirdreview.com/wp-content/uploads/2013/01/IMG_6564.jpg",
+        'scores':[1, 3, 1, 5, 2, 3, 1, 2, 1, 3,]
     }
 
 ];
